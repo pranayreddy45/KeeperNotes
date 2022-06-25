bc32967ca9ee0862f56d980fdc1cea9421c7f04e
Sai Pranay <43415112+pranayreddy45@users.noreply.github.com>
2022-06-25 13:36:56 +0530

    abc

--- app.js
+++ app.js
@@ -13,7 +13,6 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(cors());
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname,"keeper-app-frontend/build")))
 
 mongoose.connect(
   "mongodb+srv://admin-pranay:adminpranay123@cluster0.aew4g.mongodb.net/keeperApp",
